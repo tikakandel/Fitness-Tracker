900fb63f738c0830197c65f6cbaa5edade7a0a03
tikak <tika.kandel.06@gmail.com>
2021-09-26 17:39:11 +1000

    Setup server.js

---
+++ routes/apiroutes.js
@@ -0,0 +1,52 @@
+// Import express router
+const router = require("express").Router();
+
+// Import workout model
+const db = require("../models/exercise");
+
+// GET Request for getting all workouts
+router.get("/api/workouts", (req, res) => {
+	db.Workout.find()
+		.then((dbData) => {
+			res.json(dbData);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
+});
+
+// GET request
+router.get("/api/workouts/range", (req, res) => {
+	db.Workout.find()
+		.then((dbData) => {
+			res.json(dbData);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
+});
+
+// POST workout
+router.post("/api/workouts", ({ body }, res) => {
+	db.Workout.create(body)
+		.then((dbData) => {
+			res.json(dbData);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
+});
+
+// PUT/Update workout
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+	db.Workout.findByIdAndUpdate(params.id, { $push: { exercises: body } })
+		.then((dbData) => {
+			res.json(dbData);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
+});
+
+// Export API routes
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,33 @@
+
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+
+const PORT = process.env.PORT || 3000;
+
+// Create Express app
+const app = express();
+
+// app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// Create mongoose database connection
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnesstracker", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/apiroutes.js"));
+app.use(require("./routes/htmlroutes.js"));
+
+// Listen to request 
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
